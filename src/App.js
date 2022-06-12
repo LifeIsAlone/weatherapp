4f90eb934e2e62bf03b3b3966af62ba4be48dbce
[SW2/서울/1반] 박재현 <jaehyeonp74@gmail.com>
2022-06-12 12:24:09 +0900

    현재 위치의 기상, 날씨 fetch 이용해서 구현

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import "./App.css";
 import React, { useEffect, useState } from "react";
-
-function App() {
+import Weather from "./components/Weather";
+export default function App() {
   const [lat, setLat] = useState([]);
   const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      setLat(position.coords.latitude);
-      setLong(position.coords.longitude);
-    });
-    console.log("latitude is", lat);
-    console.log("longitude is", long);
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
+
+      await fetch(
+        `${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`
+      )
+        .then((res) => res.json())
+        .then((result) => {
+          setData(result);
+          console.log(result);
+        });
+    };
+    fetchData();
   }, [lat, long]);
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {typeof data.main != "undefined" ? (
+        <Weather weatherData={data} />
+      ) : (
+        <div></div>
+      )}
+    </div>
+  );
 }
-
-export default App;
---
+++ src/components/Weather.js
@@ -0,0 +1,3 @@
+const Weather = () => {};
+
+export default Weather;
