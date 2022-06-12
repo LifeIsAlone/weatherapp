11b78d900ad8a5c5eed0a373034c506c8aaf43ce
[SW2/서울/1반] 박재현 <jaehyeonp74@gmail.com>
2022-06-12 10:58:27 +0900

    현재 위치의 위도, 경도 알려주는 Hook 구현

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import "./App.css";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    });
+    console.log("latitude is", lat);
+    console.log("longitude is", long);
+  }, [lat, long]);
+
   return <div className="App"></div>;
 }
 
