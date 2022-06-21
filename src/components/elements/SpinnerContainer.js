ec619b6c731798dbabf9590d28bc9cefa1fbca5d
[SW2/서울/1반] 박재현 <jaehyeonp74@gmail.com>
2022-06-21 19:51:11 +0900

    header component

---
+++ src/components/elements/SpinnerContainer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SpinnerContainer = () => {
+  return <div></div>;
+};
+
+export default SpinnerContainer;
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -37,9 +37,36 @@ const Header = ({
         </span>
       </div>
       <div className="temperature">
-        <div>
-          <span className="degree"></span>
+        <div
+          className={`celsius ${unitMode ? "" : "active"}`}
+          onClick={() => changedTemp(false)}
+        >
+          <span className="degree">°</span> C
         </div>
+        <div
+          className={`fahrenheit ${unitMode ? "active" : ""}`}
+          onClick={() => changedTemp(true)}
+        >
+          <span className="degree">°</span> F
+        </div>
+      </div>
+      <div className="toggle-theme">
+        <input
+          type="checkbox"
+          className="checkbox"
+          id="chk"
+          checked={nightMode}
+          onChange={nightModeCallback}
+        />
+        <label htmlFor="chk" className="label">
+          <div className="sun">
+            <IoIosSunny size={15} />
+          </div>
+          <div className="moon">
+            <RiMoonClearFill size={12} />
+          </div>
+          <div className="ball"></div>
+        </label>
       </div>
     </div>
   );
