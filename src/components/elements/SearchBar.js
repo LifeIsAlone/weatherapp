d8107316b158ba6112c4b0a30ae602775dd7254b
[SW2/서울/1반] 박재현 <jaehyeonp74@gmail.com>
2022-06-21 15:23:09 +0900

    SearchBar 요소

--- src/components/elements/SearchBar.js
+++ src/components/elements/SearchBar.js
@@ -1,5 +1,40 @@
 import React, { useState } from "react";
+import { MdGpsFixed } from "react-icons/md";
+import { BiSearch } from "react-icons/bi";
 
-const SearchBar = ({ findCoordinates, searchCallback, error }) => {};
+const SearchBar = ({ findCoordinates, searchCallback, error }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const doChangeInput = (e) => {
+    setSearchTerm(e.target.value);
+  };
+  const doSearch = () => {
+    searchCallback(searchTerm);
+    setSearchTerm("");
+  };
+  return (
+    <div className="search__bar">
+      <div className="search__content">
+        <div className="search__color">
+          <BiSearch />
+        </div>
+        <div className="search__box">
+          <input
+            type="text"
+            placeholder="검색"
+            onChange={doChangeInput}
+            onKeyPress={(e) => {
+              return e.key === "Enter" && doSearch();
+            }}
+            value={searchTerm}
+          />
+        </div>
+        <div className="gray-border" onClick={findCoordinates}>
+          <MdGpsFixed />
+        </div>
+      </div>
+      {error && <div className="error">바르게 입력해주세요!</div>}
+    </div>
+  );
+};
 
 export default SearchBar;
