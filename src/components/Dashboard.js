2ba3be10a3da370ecb24e36438ed763376fcf597
[SW2/서울/1반] 박재현 <jaehyeonp74@gmail.com>
2022-06-21 14:13:31 +0900

    useTempUnit Hook + Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -14,18 +14,97 @@ import { useImageFetch } from "../hooks/useImageFetch";
 import { useNightMode } from "../hooks/useNightMode";
 import { useTempUnit } from "../hooks/useTempUnit";
 import { SEARCH_BY_WORD } from "../api";
+import { useWeatherFetch } from "../hooks/useWeatherFetch";
+import { useCoordinations } from "../hooks/useCoordinates";
 
 const Dashboard = () => {
+  const [nightMode, nightModeChanged] = useNightMode();
+  const [unitMode, unitModeChanged] = useTempUnit();
+  const [image, fetchImage] = useImageFetch();
+  const [{ lat, long }, loadingLocation, findCoordinates] = useCoordinations();
+  const [
+    weather,
+    loading,
+    error,
+    fetchWeather,
+    searchByLocation,
+    getWeatherLocation,
+  ] = useWeatherFetch();
+  const [showDays, setShowDays] = useState(false);
+
+  const fetchCoordinates = () => {
+    findCoordinates();
+    getWeatherLocation(lat, long);
+  };
+
+  const nightModeCallback = () => {
+    nightModeChanged();
+  };
+
+  const showDaysCallback = (enabled) => {
+    setShowDays(enabled);
+  };
+
+  const doSearchLocation = (searchTerm) => {
+    searchByLocation(searchTerm);
+    fetchImage(`${SEARCH_BY_WORD}${weather.city}`);
+  };
+
+  const unitTempCallback = (enabled) => {
+    unitModeChanged(enabled);
+  };
+
+  console.log("location", lat, long);
+  console.log("weather", weather);
+
+  useEffect(() => {
+    getWeatherLocation(lat, long);
+    fetchImage(`${SEARCH_BY_WORD}${weather.city}`);
+  }, [lat, long]);
+
+  if (!weather) {
+    return (
+      <ThemeProvider theme={nightMode ? themeDark : themeLight}>
+        <Spinner />
+        <StyledGlobal />
+      </ThemeProvider>
+    );
+  }
   return (
-    <ThemeProvider>
+    <ThemeProvider theme={nightMode ? themeDark : themeLight}>
       <StyledDashboard>
-        <Sidebar />
+        <Sidebar
+          findCoordinates={fetchCoordinates}
+          data={weather}
+          searchCallback={doSearchLocation}
+          image={image}
+          error={error}
+          titleLocation={weather}
+          unitTemp={unitMode}
+        />
         <Container>
-          <Header />
-          <Week />
-          <Today />
-          <Highlights />
+          <Header
+            unitMode={unitMode}
+            unitTempCallback={unitTempCallback}
+            nightModeCallback={nightModeCallback}
+            nightMode={nightMode}
+            showDaysCallback={showDaysCallback}
+            showActive={showDays}
+          />
+          {loading || loadingLocation ? (
+            <SpinnerContainer />
+          ) : (
+            <>
+              {!showDays ? (
+                <Week data={weather.daily} tempUnit={unitMode} />
+              ) : (
+                <Today tempUnit={unitMode} data={weather.hourly} />
+              )}
+              <Highlights data={weather.current} />
+            </>
+          )}
         </Container>
+        <StyledGlobal />
       </StyledDashboard>
     </ThemeProvider>
   );
